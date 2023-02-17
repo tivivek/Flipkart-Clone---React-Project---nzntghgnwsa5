51c4f56d16bce2e8d072888d6e9e41a372f96a00
Vishesh-Kumar <19vishesh@gmail.com>
2023-02-17 08:36:30 +0530

    c

---
+++ src/components/home/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, styled, Typography } from '@mui/material'
+import { navData } from '../../constants/data'
+
+
+const Component = styled(Box)`
+    display:flex;
+    margin : 55px 130px 0 130px;
+    justify-content : space-between;
+
+`
+const Container = styled(Box)`
+    padding: 12px 8px;
+    text-align: center;
+`
+const Text = styled(Typography)`
+    font-size: 14px;
+    font-weight: 600;
+    font-family: inherit
+`
+
+// funtion starts
+const NavBar = () => {
+  return (
+      <Component>
+          {
+              navData.map(data => (
+                  <Container>
+                      <img src={data.url} alt="nav" style={{width:64}} />
+                      <Text>{data.text}</Text>
+                  </Container>
+              ))
+          }
+      </Component>
+  )
+}
+
+export default NavBar
