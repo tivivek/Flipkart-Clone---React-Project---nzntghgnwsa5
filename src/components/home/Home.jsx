d206f74c37a1ef45eac0428ccec2d1fe0a1256da
Vishesh-Kumar <19vishesh@gmail.com>
2023-02-17 08:33:08 +0530

    till NavBar(some carousel)

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Banner from './Banner'
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Banner/>
+    </>
+    
+  )
+}
+
+export default Home
